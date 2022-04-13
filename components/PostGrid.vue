7d9f48240f153e25309d06578b4a368c1d75616d
Vlad <clay.zenx@gmail.com>
2022-04-13 15:05:14 +0700

    Post grid

---
+++ components/PostGrid.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.grid {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 23px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,12 @@
 const config = useRuntimeConfig();
 let page = ref(1);
 const { data } = await useFetch(() => `posts?_page=${page.value}&take=9&_embed=comments`, { baseURL: config.API_URL });
-
 </script>
 
 <template>
-  <Post v-if="data" v-for="post in data" :post="post" />
+  <PostGrid v-if="data">
+    <Post v-for="post in data" :post="post" />
+  </PostGrid>
 </template>
 
 
