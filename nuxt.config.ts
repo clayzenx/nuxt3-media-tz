230316271b1c61e6bb8f2c2b781dc958a662758c
Vlad <clay.zenx@gmail.com>
2022-04-13 13:28:49 +0700

    Add assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL,
+  },
+  css: ["@/assets/global.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: '@import "@/assets/_variables.scss"',
+        },
+      },
+    },
+  },
 })
